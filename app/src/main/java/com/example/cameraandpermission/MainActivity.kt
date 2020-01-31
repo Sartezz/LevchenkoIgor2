dc4c85e8db914ae596c7874551d42b0f21734926
Igor Levchenko <i2.levchenko@itransition.com>
2020-01-31 18:57:13 +0300

    asdfgh

---
+++ app/src/main/java/com/example/cameraandpermission/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.cameraandpermission
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.net.Uri.fromParts
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
+
+
+const val CAMERA_PERMISSION_CODE = 1
+
+class MainActivity : AppCompatActivity() {
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewFinder = view_finder
+
+        button.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                viewFinder.post { startCamera() }
+            } else {
+                requestPermission()
+            }
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            AlertDialog.Builder(this).setTitle("Permission needed")
+                .setMessage("you need it because of this reason")
+                .setPositiveButton("ok") { dialog, which ->
+                    ActivityCompat.requestPermissions(
+                        this, arrayOf(Manifest.permission.CAMERA),
+                        CAMERA_PERMISSION_CODE
+                    )
+                }
+                .setNegativeButton("cancel") { dialog, which ->
+                    dialog.dismiss()
+                }
+                .create().show()
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                viewFinder.post { startCamera() }
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    Toast.makeText(this, "asdasdasd granted", Toast.LENGTH_LONG).show()
+
+                }
+
+            }
+        }
+    }
+
+    private fun startCamera() {
+        Log.d("aaa","aaa")
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(1920, 1080))
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+        CameraX.bindToLifecycle(this, preview)
+
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        viewFinder.setTransform(matrix)
+    }
+}
